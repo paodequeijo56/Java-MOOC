dd3724e10a3ac3c6b2578af1061b9836872c8c5f
Gabi <146675101+paodequeijo56@users.noreply.github.com>
2023-10-14 17:37:04 +0200

    Create james-bond.java]

---
+++ james-bond.java
@@ -0,0 +1,9 @@
+public class Bond {
+    
+    public static void main(String[] args) {
+        String start = "My name is ";
+        String end = ", James Bond";
+
+        System.out.println(start + "Bond" + end);
+    }
+}
